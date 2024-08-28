bf38080ec2188ead0938d07bdc73955fc437658b
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2024-08-29 00:41:45 +0530

    create a user model

---
+++ backend/src/user/userModel.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import { User } from "./userTypes";
+
+const userSchema = new mongoose.Schema<User>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
