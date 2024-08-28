6aa50ef81a17ce5a4a3b5537031591a44fb8da64
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2024-08-29 01:17:27 +0530

    hashed the password using bcrypt

--- backend/src/user/userController.ts
+++ backend/src/user/userController.ts
@@ -1,19 +1,41 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import User from "./userModel";
+import bcrypt from "bcrypt"
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { name, email, password } = req.body;
 
-    const{name, email, password} = req.body;
-  
   // validation
-  // you can use also an express-validation library for validation purpose
-   if(!name || !email || !password) {
-    const error = createHttpError(400, 'All fields are required');
-     return next(error)
-   }
-  // process 
-  // response 
+  // you can use also an express-validation library for validation purpose for better validation
+  if (!name || !email || !password) {
+    const error = createHttpError(400, "All fields are required");
+    return next(error);
+  }
+
+  // database call- user exist or not .
+  const user = await User.findOne({ email });
+  if (user) {
+    const error = createHttpError(401, "User already exists with this email");
+    return next(error);
+  }
+
+  // when create a user in database then first of all hashed the password via bcrypt library 
+
+  const hashedPassword = bcrypt.hashSync(password,10);
+  
+
+
+
+
+
+
+
+
+
+  // process
+  // response
 };
 
-
-export {createUser}
+export { createUser };
+  
